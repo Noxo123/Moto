d5a3a5c8f4186aae300c90a6612f3b5656ba5b21
Noxo123 <138624794+Noxo123@users.noreply.github.com>
2024-12-08 17:38:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
-// Configuration Firebase
+// firebase-config.js
+import { initializeApp } from 'firebase/app';
+
 const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "ton-projet.firebaseapp.com",
-    projectId: "ton-projet",
-    storageBucket: "ton-projet.appspot.com",
-    messagingSenderId: "ID",
-    appId: "ID",
-    measurementId: "ID"
+    apiKey: "AIzaSyDrwlY5bFSGkq7wXveKZUza_1NEmMnNVZ4",
+    authDomain: "application-dfc1c.firebaseapp.com",
+    databaseURL: "https://application-dfc1c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "application-dfc1c",
+    storageBucket: "application-dfc1c.firebasestorage.app",
+    messagingSenderId: "539415401283",
+    appId: "1:539415401283:web:5fac4a6d3c0a9ae0f155f6",
+    measurementId: "G-JX0DWX7JCR"
 };
 
-// Initialisation de Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(app);
-const auth = firebase.auth(app);
-const storage = firebase.storage();
+export const app = initializeApp(firebaseConfig);
