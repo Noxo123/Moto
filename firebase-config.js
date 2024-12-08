d4600b2a5d7465836c0026b3fa9e5a6212a05ffa
Noxo123 <138624794+Noxo123@users.noreply.github.com>
2024-12-08 17:48:06 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,17 @@
 // firebase-config.js
 import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrwlY5bFSGkq7wXveKZUza_1NEmMnNVZ4",
-    authDomain: "application-dfc1c.firebaseapp.com",
-    databaseURL: "https://application-dfc1c-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "application-dfc1c",
-    storageBucket: "application-dfc1c.firebasestorage.app",
-    messagingSenderId: "539415401283",
-    appId: "1:539415401283:web:5fac4a6d3c0a9ae0f155f6",
-    measurementId: "G-JX0DWX7JCR"
+  apiKey: "AIzaSyDrwlY5bFSGkq7wXveKZUza_1NEmMnNVZ4",
+  authDomain: "application-dfc1c.firebaseapp.com",
+  databaseURL: "https://application-dfc1c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "application-dfc1c",
+  storageBucket: "application-dfc1c.firebasestorage.app",
+  messagingSenderId: "539415401283",
+  appId: "1:539415401283:web:5fac4a6d3c0a9ae0f155f6",
+  measurementId: "G-JX0DWX7JCR"
 };
 
 export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
